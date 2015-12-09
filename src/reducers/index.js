448562c43480e94ce6ab52bf8c895a63acd57479
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-12-09 21:42:12 +0900

    Support more action.type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { VisibilityFilters } from '../actions';
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER } from '../actions/actionTypes';
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -6,5 +7,32 @@ const initialState = {
 };
 
 function todoApp(state = initialState, action = {}) {
-  return state;
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign( {}, state, {
+        visibilityFilter: action.filter
+      });
+    case ADD_TODO:
+      return Object.assign( {}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      });
+    case COMPLETE_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos.slice(0, action.index),
+          Object.assign({}, state.todos[action.index], {
+            completed: true
+          }),
+          ...state.todos.slice(action.index + 1)
+        ]
+      });
+    default:
+      return state; 
+  }
 }
