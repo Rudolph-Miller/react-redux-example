4584638d0dda93ad56ad8b12f596995402b88bb5
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-12-09 21:52:35 +0900

    Add stores

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux';
+import todoApp from './reducers';
+
+let store = createStore(todoApp);
 
 class App extends React.Component {
   constructor(props) {
