d13e963ce54a970b0896150543c954481c7be674
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-12-10 09:34:12 +0900

    Tweak

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,7 +5,23 @@ import TodoList from '../components/TodoList';
 import Footer from '../components/Footer';
 import { VisibilityFilters, addTodo, completeTodo, setVisibilityFilter } from '../actions';
 
-export default class App extends Component {
+const filters = [];
+for ( let key in VisibilityFilters ) {
+  filters.push(VisibilityFilters[key]);
+}
+
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed);
+  }
+}
+
+class App extends Component {
   render() {
     const { dispatch, visibleTodos, visibilityFilter } = this.props;
     return (
@@ -23,11 +39,6 @@ export default class App extends Component {
   }
 }
 
-const filters = [];
-for ( let key in VisibilityFilters ) {
-  filters.push(VisibilityFilters[key]);
-}
-
 App.propTypes = {
   visibleTodos: PropTypes.arrayOf(PropTypes.shape({
     text: PropTypes.string.isRequired,
@@ -36,22 +47,9 @@ App.propTypes = {
   visibilityFilter: PropTypes.oneOf(filters).isRequired
 };
 
-function selectTodos(todos, filter) {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos;
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(todo => todo.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.completed);
-  }
-}
-
-function select(state) {
+export default connect(state => {
   return {
     visibleTodos: selectTodos(state.todos, state.visibilityFilter),
     visibilityFilter: state.visibilityFilter
   };
-}
-
-export default connect(select)(App);
+})(App);
